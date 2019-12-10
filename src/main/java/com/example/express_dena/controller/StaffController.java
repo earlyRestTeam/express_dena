f385b350d43924dfc6e4c86b60ba60048cb54fff
yang199865 <2205348277@qq.com>
2019-12-10 21:50:37 +0800

    骑手页面controller上传

---
+++ src/main/java/com/example/express_dena/controller/StaffController.java
@@ -0,0 +1,63 @@
+package com.example.express_dena.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author :Yang Jiahong
+ * @date :2019/12/10 20:58
+ */
+@Controller
+@RequestMapping("/staff")
+public class StaffController {
+
+    //登录页面跳转
+    @RequestMapping("/login")
+    public String stafflogin(){
+        return "/login";
+    }
+
+    //注册界面跳转
+    @RequestMapping("/register")
+    public String staffMain(){
+        return "/register";
+    }
+
+    //快递员钱包界面跳转
+    @RequestMapping("/wallet")
+    public String staffWaller(){
+        return "/staff/wallet";
+    }
+
+    //快递员个人信息
+    @RequestMapping("/staffinfo")
+    public String staffInfo(){
+        return "/staff/staffinfo";
+    }
+
+    //历史订单界面
+    @RequestMapping("/HistoryOrder")
+    public String staffHistoryOrder(){
+
+        return "/staff/HistoryOrder";
+    }
+
+    //更改密码界面
+    @RequestMapping("/changePwd")
+    public String staffchangePwd(){
+        return "/staff/changePwd";
+    }
+
+    //当订单详情页面
+    @RequestMapping("/detailsOrder")
+    public String detailsOrder(){
+        return "/staff/detailsOrder";
+    }
+
+    //当前订单跳转1
+    @RequestMapping("/currentStaffOrder")
+    public String currentStaffOrder(){
+        return "/staff/currentStaffOrder";
+    }
+
+}
