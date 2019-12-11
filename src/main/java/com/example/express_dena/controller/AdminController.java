aaf1aa19bdee1b811304d7e2fac1b3732f6e3d80
zzz1040592536 <1040592536@qq.com>
2019-12-11 10:15:48 +0800

    admin静态资源重新上传，adminController路径返回的_改为-

--- src/main/java/com/example/express_dena/controller/AdminController.java
+++ src/main/java/com/example/express_dena/controller/AdminController.java
@@ -21,48 +21,48 @@ public class AdminController {
     
     @RequestMapping("admin_add")
     public String admin_add(){
-        return "/admin/admin_add";
+        return "/admin/admin-add";
     }
 
     @RequestMapping("admin_list")
     public String admin_list(){
-        return "/admin/admin_list";
+        return "/admin/admin-list";
     }
 
     @RequestMapping("admin_password_edit")
     public String admin_password_edit(){
-        return "/admin/admin_password_edit";
+        return "/admin/admin-password-edit";
     }
 
     @RequestMapping("admin_permission")
     public String admin_permission(){
-        return "/admin/admin_permission";
+        return "/admin/admin-permission";
     }
 
     @RequestMapping("admin_role")
     public String admin_role(){
-        return "/admin/admin_role";
+        return "/admin/admin-role";
     }
 
     @RequestMapping("admin_role_add")
     public String admin_role_add(){
-        return "/admin/admin_role_add";
+        return "/admin/admin-role-add";
     }
 
     @RequestMapping("article_list")
     public String article_list(){
-        return "/admin/article_list";
+        return "/admin/article-list";
     }
 
     @RequestMapping("change_password")
     public String change_password(){
-        return "/admin/change_password";
+        return "/admin/change-password";
     }
 
 
     @RequestMapping("feedback_list")
     public String feedback_list(){
-        return "/admin/feedback_list";
+        return "/admin/feedback-list";
     }
 
     @RequestMapping("index")
@@ -72,71 +72,71 @@ public class AdminController {
 
     @RequestMapping("member_add")
     public String member_add(){
-        return "/admin/member_add";
+        return "/admin/member-add";
     }
 
 
 
     @RequestMapping("member_del")
     public String member_del(){
-        return "/admin/member_del";
+        return "/admin/member-del";
     }
 
     @RequestMapping("member_list")
     public String member_list(){
-        return "/admin/member_list";
+        return "/admin/member-list";
     }
 
     @RequestMapping("picture_add")
     public String picture_add(){
-        return "/admin/picture_add";
+        return "/admin/picture-add";
     }
 
     @RequestMapping("picture_list")
     public String picture_list(){
-        return "/admin/picture_list";
+        return "/admin/picture-list";
     }
 
     @RequestMapping("picture_show")
     public String picture_show(){
-        return "/admin/picture_show";
+        return "/admin/picture-show";
     }
 
     @RequestMapping("record_browse")
     public String record_browse(){
-        return "/admin/record_browse";
+        return "/admin/record-browse";
     }
 
     @RequestMapping("record_download")
     public String record_download(){
-        return "/admin/record_download";
+        return "/admin/record-download";
     }
 
     @RequestMapping("record_share")
     public String record_share(){
-        return "/admin/record_share";
+        return "/admin/record-share";
     }
 
     @RequestMapping("user_add")
     public String user_add(){
-        return "/admin/user_add";
+        return "/admin/user-add";
     }
 
     @RequestMapping("user_list")
     public String user_list(){
-        return "/admin/user_list";
+        return "/admin/user-list";
     }
 
     @RequestMapping("user_password_edit")
     public String user_password_edit(){
-        return "/admin/user_password_edit";
+        return "/admin/user-password-edit";
     }
 
 
 
     @RequestMapping("user_show")
     public String user_show(){
-        return "/admin/user_show";
+        return "/admin/user-show";
     }
 
     @RequestMapping("welcome")
