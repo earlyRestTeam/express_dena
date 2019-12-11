13eccd5014f12b4e17009cc1da8e6daa8b40df8b
WanT0011 <2374909509@qq.com>
2019-12-11 08:59:23 +0800

    增加 util 包

---
+++ src/main/java/com/example/express_dena/util/StaticPool.java
@@ -0,0 +1,10 @@
+package com.example.express_dena.util;
+
+/**
+ * @author 王志坚
+ * @createTime 2019.12.10.21:43
+ */
+public class StaticPool {
+    public static String ERROR = "error";
+    public static String SUCCESS = "success";
+}
