7c5075fc1307ae7eddd538cf7c755c1142af3b88
WanT0011 <2374909509@qq.com>
2019-12-11 08:52:39 +0800

    增加用户的部分服务层 -- 并全部通过测试

--- src/main/java/com/example/express_dena/services/UserService.java
+++ src/main/java/com/example/express_dena/services/UserService.java
@@ -1,13 +1,61 @@
 package com.example.express_dena.services;
 
+import com.example.express_dena.pojo.Horseman;
+import com.example.express_dena.pojo.User;
+
+import java.util.Map;
+
 public interface UserService {
     int insert();
 
+    /**
+     * 注册 user
+     * @param user
+     * @return
+     */
+    Map<String,String> register(User user);
 
+    /**
+     * 修改 用户信息
+     * @param user
+     * @return
+     */
+    Map<String,String> updateUserInfo(User user);
 
+    /**
+     * 修改 用户密码
+     * @param userid
+     * @param oldPassword
+     * @param newPassword
+     * @return
+     */
+    Map<String,String> changePassword(int userid,String oldPassword,String newPassword);
 
+    /**
+     * 找回密码
+     * @param emailCode
+     * @param newPassword
+     * @param account
+     * @param code
+     * @return
+     */
+    Map<String,String> forgetPassword(String emailCode,String newPassword
+            ,String account,String code);
 
+    /**
+     * 认证 成为骑手
+     * @param horseman
+     * @return
+     */
+    Map<String,String> applicationStaff(Horseman horseman);
 
+    /**
+     * 充值
+     * @param num
+     * @return
+     */
+    Map<String,String> recharge(float num);
 
+    Map<String,String> feedBack(String content,int orderid);
 
 }
