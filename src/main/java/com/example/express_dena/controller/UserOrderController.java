ec1a4321b5e37032b4d45a5e50cae6c3e4994e59
yang199865 <2205348277@qq.com>
2019-12-11 16:30:52 +0800

    用户订单service和controller

---
+++ src/main/java/com/example/express_dena/controller/UserOrderController.java
@@ -0,0 +1,32 @@
+package com.example.express_dena.controller;
+
+import com.example.express_dena.services.UserOrderService;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author :Yang Jiahong
+ * @date :2019/12/11 16:03
+ */
+@RequestMapping("/staff")
+@Controller
+public class UserOrderController {
+
+    @Autowired
+    UserOrderService service;
+
+
+    @RequestMapping("selectOrderCurrent")
+    public String selectOrderCurrent(int userid, Integer indexpage, HttpServletRequest request){
+        PageInfo info = service.selectOrderCurrent(userid,indexpage);
+        request.setAttribute("page",info);
+        return "/staff/currentStaffOrder";
+
+    }
+
+}
