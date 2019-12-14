cdc9e62fbcd5e25d0075d22f1faf58cb0d2635ac
WanT0011 <2374909509@qq.com>
2019-12-14 11:42:11 +0800

    首页 加 骑手 登陆 的 修正 -- 王志坚

--- src/main/java/com/example/express_dena/controller/interceptor/EntityInterceptor.java
+++ src/main/java/com/example/express_dena/controller/interceptor/EntityInterceptor.java
@@ -1,14 +1,19 @@
 package com.example.express_dena.controller.interceptor;
 
+import com.example.express_dena.pojo.Admin;
+import com.example.express_dena.pojo.User;
 import com.example.express_dena.security.LoginEntityHelper;
 import com.example.express_dena.security.LoginUrlEntryPoint;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Collection;
 
 /**
  * @author 王志坚
@@ -30,8 +35,18 @@ public class EntityInterceptor implements HandlerInterceptor {
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
         Object o = loginEntityHelper.getEntity();
         if( !(o instanceof String) ) //        "anonymousUser"
-            if(modelAndView != null)
+            if(modelAndView != null){
+                int type;
+                if(o instanceof User)
+                    type = 1;
+                else if(o instanceof Admin)
+                    type = 2;
+                else
+                    type = 3;
+                modelAndView.addObject("type",type);
                 modelAndView.addObject("entity",o);
+            }
+
     }
 
     @Override
